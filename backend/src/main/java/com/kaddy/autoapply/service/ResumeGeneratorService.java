549d62d2861ac7d13edc7dac1f675d9eb7bf43b6
Rajesh Singh Kadyan <rajeshsinghkadyan@gmail.com>
2026-04-13 12:15:16 +0530

    Build Stage Patched

--- backend/src/main/java/com/kaddy/autoapply/service/ResumeGeneratorService.java
+++ backend/src/main/java/com/kaddy/autoapply/service/ResumeGeneratorService.java
@@ -9,6 +9,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
